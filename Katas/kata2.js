279a5acab84b7f74cd9a75eb94a061302021a799
Pablo López <p.lopezmunoz@gmail.com>
2022-01-18 09:50:38 +0100

    kata solved

--- Katas/kata2.js
+++ Katas/kata2.js
@@ -1,20 +1,7 @@
 function specialNumber(n){
-    if (n.toString().length === 1){
-      if (n === 0 || n === 1 || n === 2 || n === 3 || n === 4 || n === 5){
-        return 'Special!!';
-      } else {
-        return 'NOT!!'
-      }
-    } else {
-      let numStringArr = n.toString().split('');
-      for (let i = 0; i < numStringArr.length; i ++){
-        if (numStringArr[i] === '0' || numStringArr[i] === '1' || numStringArr[i] === '2' || numStringArr[i] === '3' || numStringArr[i] === '4' || numStringArr[i] === '5'){
-          return 'Special!!'
-        } else if (numStringArr[i] !== '0' || numStringArr[i] !== '1' || numStringArr[i] !== '2' || numStringArr[i] !== '3' || numStringArr[i] !== '4' || numStringArr[i] !== '5'){
-          return 'Perro!!'
-        }
-      }
-    }
+    for (let i = 0; i < n.toString().length; i++){
+      if (n.toString()[i] > '5'){
+        return "NOT!!";
+      } 
+    } return "Special!!";
   }
-    
-    console.log(specialNumber(65));
